b703fb8031fe35a3106b6d1d5db826b098d899de
Vin2nt <vincent@samsz.de>
2025-02-14 11:35:19 -0500

    Add initial project setup with configuration files and robot code

---
+++ src/main/java/frc/robot/subsystems/CANRollerSubsystem.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.RollerConstants;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+/** Class to run the rollers over CAN */
+public class CANRollerSubsystem extends SubsystemBase {
+  VictorSPX rollerMotor;
+
+  public CANRollerSubsystem() {
+    // Set up the roller motor as a brushed motor
+    rollerMotor = new VictorSPX(RollerConstants.ROLLER_MOTOR_ID);
+
+    // Set can timeout. Because this project only sets parameters once on
+    // construction, the timeout can be long without blocking robot operation. Code
+    // which sets or gets parameters during operation may need a shorter timeout.
+    // rollerMotor.setCANTimeout(250);
+
+    // Create and apply configuration for roller motor. Voltage compensation helps
+    // the roller behave the same as the battery
+    // voltage dips. The current limit helps prevent breaker trips or burning out
+    // the motor in the event the roller stalls.
+    // SparkMaxConfig rollerConfig = new SparkMaxConfig();
+    // rollerConfig.voltageCompensation(RollerConstants.ROLLER_MOTOR_VOLTAGE_COMP);
+    // rollerConfig.smartCurrentLimit(RollerConstants.ROLLER_MOTOR_CURRENT_LIMIT);
+    // rollerMotor.configure(rollerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  @Override
+  public void periodic() {
+  }
+
+  /** This is a method that makes the roller spin */
+  public void runRoller(double forward, double reverse) {
+    rollerMotor.set(ControlMode.PercentOutput, forward - reverse);
+  }
+}
